cb41303f47ea92e0d05360b470a3818c12f337c4
root <root@mail.moxixii.com>
2021-02-03 01:39:14 +0800

    modify error num to 5

--- searchAttackerIp.sh
+++ searchAttackerIp.sh
@@ -8,10 +8,10 @@ LOGFILE="/var/log/maillog"
 # 统计maillog中authentication failure的IP个数和IP
 grep "authentication failure" $LOGFILE | awk '{print $7}' | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | sort | uniq -c > af_iplist.txt
 
-# 取出AF出现大于50次的IP
-awk '$1 > 50 {print $2}' af_iplist.txt > block_ip_list.txt
+# 取出AF出现大于5次的IP
+awk '$1 > 5 {print $2}' af_iplist.txt > block_ip_list.txt
 
-# 大于50次AF的IP添加到iptables中
+# 大于5次AF的IP添加到iptables中
 cat block_ip_list.txt | while read line
 do
 	/sbin/iptables -nL | grep $line
